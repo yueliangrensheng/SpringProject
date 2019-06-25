56844ecfced07b396cec416c6234a321cc81f7a4
yueliangrensheng <shaopingzhai@163.com>
2019-06-25 15:09:03 +0800

    SpringMVC Controller方法中接受参数

--- src/com/yazao/springmvc/controller/ItemController2.java
+++ src/com/yazao/springmvc/controller/ItemController2.java
@@ -2,9 +2,14 @@ package com.yazao.springmvc.controller;
 
 import com.yazao.springmvc.domain.Items;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -37,5 +42,30 @@ public class ItemController2{
         //通过在springmvc.xml中配置视图解析器的prefix和suffix，来在code中简写配置视图的路径
         modelAndView.setViewName("itemList");
         return modelAndView;
+    }
+
+    //SpringMVC的Controller方法中接受参数：
+
+    //1. SpringMVC 默认绑定 参数(可选，可有可无) :HttpServletRequest request, HttpServletResponse response, HttpSession session, Model model
+    //2. 基本数据类型 --- 页面参数的名称和形参的名称一致。
+    //   如果不一致，辣么需要 @RequestParam(value="页面参数名称")
+
+    // 请求url： http://localhost:8080/SpringWeb/list.action?id=9
+    @RequestMapping("/list3")
+    public String listItem(@RequestParam(value = "id") int itemId, HttpServletRequest request, HttpServletResponse response, HttpSession session, Model model){
+        //模拟数据
+        List<Items> list = new ArrayList<Items>();
+
+        for (int i = 0; i < 10; i++) {
+            Items items = new Items();
+            items.setId(i);
+            items.setCreatetime(new Date());
+            items.setName("华为手机" + i);
+            items.setDetail("国产");
+            items.setPrice((float) (1000 * i));
+            list.add(items);
+        }
+
+        return "itemList";
     }
 }
